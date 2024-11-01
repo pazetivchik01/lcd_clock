e2a0c1dab5ff3c961106a41720d4f5f838c1df05
pazetivchik01 <romanenkodenisnadrwivi@gmail.com>
2024-11-01 17:49:43 +0300

    old logic was update

--- frimware/clock_1602..ino
+++ frimware/clock_1602..ino
@@ -10,6 +10,11 @@ const char* pass = "your_pass";
 struct tm timeinfo;
 
 
+int lcds_time = 3399;
+int real_time = 0;
+
+
+
 LiquidCrystal_I2C lcd(0x27, 16,2);
 
 byte LT[] = {B00111,  B01111,  B11111,  B11111,  B11111,  B11111,  B11111,  B11111};
@@ -33,75 +38,77 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Подключение к Wi-Fi...");
+    lcd.setCursor(0,0);
+    lcd.print("Connecting...");
   }
+  lcd.clear();
+  lcd.print("Connected");
+  lcd.setCursor(0,1);
+  lcd.print("successfully");
   Serial.println("Подключено к Wi-Fi");
-
-
+  
   configTime(3 * 3600, 0, "time.google.com", "time.nist.gov");
-
-delay(1000);
-
-    
+  delay(1000);
+  
   while(!getLocalTime(&timeinfo)) {
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("ERROR");
     lcd.setCursor(0,1);
-    lcd.print("FAILD TO GET TIME");
-    Serial.println("неудалось получить время (failed to get time)");
-
+    lcd.print("FAILD GET TIME");
+    Serial.println("Не удалось получить время (failed to get time)");
     delay(1000);
   }
 
   WiFi.disconnect();
   
-      Serial.print("Текущее время: ");
-      Serial.print(asctime(&timeinfo));
+  Serial.print("Текущее время: ");
+  Serial.print(asctime(&timeinfo));
 
-  
-  init_digits();
-  draw_digit(0,0);// digit , colounm, default row  - 0
-  draw_digit(1,3);
-  draw_digit(2,6);
-  draw_digit(3,9);
+  init_digits(); // initialization digits
+  lcd.clear();
 }
 
 void loop() {
-
-  lcd.clear();
-
   if (!getLocalTime(&timeinfo)) {
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("ERROR");
     lcd.setCursor(0,1);
     lcd.print("FAILD GET TIME");
-    Serial.println("неудалось получить время (failed to get time)");
+    Serial.println("Не удалось получить время (failed to get time)");
     return;
   }
-  
   String time_p = extractTime(asctime(&timeinfo));
   time_p.remove(2, 1);
-  int i_tmp;
-  for(int i = 0; i < 4; i++){
-    if(i == 0)  i_tmp=0;
-    if(i == 1)  i_tmp=3;
-    if(i == 2)  i_tmp=7;
-    if(i == 3)  i_tmp=10;
-  
-    draw_digit(time_p.charAt(i) - '0', i_tmp);
-    
-  }
-  lcd.setCursor(6,0);
-  lcd.write(165);
-  lcd.setCursor(6,1);
-  lcd.write(165);
 
-  delay(10000);
+  real_time = ((time_p.charAt(0) - '0')*1000) + ((time_p.charAt(1) - '0')*100) + ((time_p.charAt(2) - '0')*10) + (time_p.charAt(3) - '0');
+  
+  if(real_time != lcds_time){
+    if(real_time/1000 != lcds_time/1000)
+      draw_digit(real_time/1000, 0);
+      
+    if(real_time/100%10 != lcds_time/100%10)
+      draw_digit(real_time/100%10, 3);
+      
+    if(real_time/10%10 != lcds_time/10%10)
+      draw_digit(real_time/10%10, 7);
+      
+    if(real_time%10 != lcds_time%10){
+      draw_digit(real_time%10, 10);
+      lcds_time = real_time;
+      }
+      else{
+        draw_digit(real_time%10, 10);
+        lcds_time = real_time;
+      }
+  }
 }
 
 void draw_digit(int dig, int x){
   switch (dig) {
+
+    // lcd.write(16);  void symbol
     case 0:
       lcd.setCursor(x, 0); 
       lcd.write(0);  
@@ -113,11 +120,15 @@ void draw_digit(int dig, int x){
       lcd.write(5);
       break;
     case 1:
-      lcd.setCursor(x + 1, 0);
+      lcd.setCursor(x, 0);
+      lcd.write(16);
       lcd.write(1);
       lcd.write(2);
-      lcd.setCursor(x + 2, 1);
+      lcd.setCursor(x, 1);
+      lcd.write(16);
+      lcd.write(16);
       lcd.write(5);
+      
       break;
     case 2:
       lcd.setCursor(x, 0);
@@ -144,7 +155,9 @@ void draw_digit(int dig, int x){
       lcd.write(3);
       lcd.write(4);
       lcd.write(2);
-      lcd.setCursor(x + 2, 1);
+      lcd.setCursor(x, 1);
+      lcd.write(16);
+      lcd.write(16);
       lcd.write(5);
       break;
     case 5:
@@ -172,8 +185,10 @@ void draw_digit(int dig, int x){
       lcd.write(1);
       lcd.write(1);
       lcd.write(2);
-      lcd.setCursor(x + 1, 1);
+      lcd.setCursor(x, 1);
+      lcd.write(16);
       lcd.write(0);
+      lcd.write(16);
       break;
     case 8:
       lcd.setCursor(x, 0);
@@ -190,7 +205,8 @@ void draw_digit(int dig, int x){
       lcd.write(0);
       lcd.write(6);
       lcd.write(2);
-      lcd.setCursor(x + 1, 1);
+      lcd.setCursor(x, 1);
+      lcd.write(16);
       lcd.write(4);
       lcd.write(5);
       break;
